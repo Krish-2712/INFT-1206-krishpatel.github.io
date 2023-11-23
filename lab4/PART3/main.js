f80c711fcc39b0c05c95f73afa08867928486126
Krish 27 Patel <krishpatel2722@gmail.com>
2023-11-23 18:04:48 -0500

    create a constructor
    A constructor named ball is being created with the needed parameters

--- lab4/PART3/main.js
+++ lab4/PART3/main.js
@@ -1,7 +1,7 @@
 // setup canvas
 
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
 
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
@@ -17,4 +17,49 @@ function random(min, max) {
 
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
+
+// create a class named ball and the needed constructors
+
+class Ball {
+  constructor(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velx;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+  }
+
+  // Now we start drawing the ball
+
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
+  // update class for updating the co ordinates of the ball
+
+  update(){
+    if ((this.x + this.y)>= width){
+      this.velX = -(this.velX);
+    }
+
+    if((this.x - this.size)<=0){
+      this.velX = -(this.velX);
+    }
+
+    if((this.y + this.size)>= height){
+      this.velY = -(this.velY);
+    }
+
+    if((this.y - this.size)<= 0){
+      this.velY = -(this.velY);
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+  }
 }
