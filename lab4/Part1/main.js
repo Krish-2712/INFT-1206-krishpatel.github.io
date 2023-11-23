741e689d13d23b5e129c581a7bffa585d43a8599
Krish 27 Patel <krishpatel2722@gmail.com>
2023-11-22 20:32:40 -0500

    changes in the second if block
    changes of the temperature and the weight madein the second block and also complted the function

--- lab4/Part1/main.js
+++ lab4/Part1/main.js
@@ -39,16 +39,20 @@ function result() {
     
     if(customName.value !== '') {
         const name = customName.value;
-        newStory = newStory.replace("Bob",name)
+        newStory = newStory.replaceAll("Bob",name)
 
 }
 
     if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
+    const weight = Math.round(300 * 0.07) + "stones";
+    newStory = newStory.replaceAll("300 pounds",weight)
+
+
+    const temperature =  Math.round((94-32)*5/9) + "centigrade";
+    newStory = newStory.replaceAll("94 fahrenheit",temperature)
 
 }
 
-    story.textContent = ;
+    story.textContent = newStory;
     story.style.visibility = 'visible';
 }
