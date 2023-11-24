d4fa35c7e6dd1b8c57ed02f12adf5e088dfc6c30
Krish 27 Patel <krishpatel2722@gmail.com>
2023-11-23 22:21:10 -0500

    boolean and update the method
    created a boolean in the ball class and make changes in the collision detect method by adding the boolean in the if to check the condition

--- lab4/part4/main-finished.js
+++ lab4/part4/main-finished.js
@@ -32,9 +32,10 @@ class Ball extends Shape {
 
    constructor(x, y, velX, velY, color, size) {
       super(x,y,velX,velY);
-      
+
       this.color = color;
       this.size = size;
+      this.exists = true;
    }
 
    draw() {
@@ -67,7 +68,7 @@ class Ball extends Shape {
 
    collisionDetect() {
       for (const ball of balls) {
-         if (!(this === ball)) {
+         if (!(this === ball) & ball.exists) {
             const dx = this.x - ball.x;
             const dy = this.y - ball.y;
             const distance = Math.sqrt(dx * dx + dy * dy);
